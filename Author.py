8fd1c471992fa6295620898af4a845cf5a4c8400
Mihai Nechita <mihai.nechita95@yahoo.com>
2018-06-11 14:20:13 -0700

    a

--- Author.py
+++ Author.py
@@ -5,9 +5,13 @@ class Author(object):
 
     def __init__(self, args):
         self.name = args[0].lower()
-        self.given_names = re.split(r'\s+', args[1].lower().replace('.', ''))
+        # self.given_names = re.split(r'\s+', args[1].lower().replace('.', ''))
+        self.given_names = args[1].lower()
 
     def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
         return '{} {}'.format(self.name, self.given_names)
 
 
